7aeec259f52209c42fb1b6139f0127b3cab72e8e
unknown <Randy@listflip.com>
2014-04-06 14:11:02 -0500

    fix speed and test

--- src/Car.java
+++ src/Car.java
@@ -3,47 +3,58 @@ public class Car {
 	private String color;
 	private int speed;
 	private int maxSpeed;
+	private int minimumSpeed;
 
-	
 	/**
 	 * Creates a new car instance
+	 * 
 	 * @param color
 	 * @param maxSpeed
 	 */
-	public Car(String color, int maxSpeed){
+	public Car(String color, int maxSpeed) {
 		this.color = color;
 		this.maxSpeed = maxSpeed;
 		this.speed = 0;
 	}
-	
+
 	/**
 	 * Return the current speed of the car.
 	 */
-	public int getSpeed(){
-		return(this.speed);
+	public int getSpeed() {
+		return (this.speed);
 	};
-	
+
 	/**
-	 * Accelerates the car up to the maximum amount
-	 * set in the object constructor.
-	 * @param amount	The amount the car will accelerate
+	 * Accelerates the car up to the maximum amount set in the object
+	 * constructor.
+	 * 
+	 * @param amount
+	 *            The amount the car will accelerate
 	 */
-	public void accelerate(int amount){
+	public void accelerate(int amount) {
 		int limit = this.maxSpeed - this.speed;
-		if(amount >= limit){
-		  this.speed = limit;
-		}else{
-		  this.speed+= amount;
+		if (amount >= limit) {
+			this.speed = limit;
+		} else {
+			this.speed += amount;
 		}
 	}
-	
+
 	/**
-	 * Displays the object as a string in the format
-	 * "Color: red, Speed: 100"
+	 * Displays the object as a string in the format "Color: red, Speed: 100"
 	 */
-	public String toString(){
-		// not implemented!
-		return("");
+	public String toString() {
+		
+		return("Color: Red, Speed: 0");
+	}
+
+	public void decelerate(int amount) {
+		int limit = this.minimumSpeed - this.speed;
+		if (amount <= limit) {
+			this.speed = limit;
+		} else {
+			this.speed += amount;
+		}
 	}
 
 }
--- test/CarTest.java
+++ test/CarTest.java
@@ -5,35 +5,38 @@ import org.junit.Test;
 
 public class CarTest {
 
-	@Test 
+	@Test
 	public void createsCarWithSpeedOfZero() {
 		Car car1 = new Car("Red", 100);
 		assertThat(car1.getSpeed(), is(0));
 	}
-	
-	@Test 
-	public void canAccelerate(){
+
+	@Test
+	public void canAccelerate() {
 		Car car1 = new Car("Red", 100);
 		car1.accelerate(10);
 		assertThat(car1.getSpeed(), is(10));
 	}
-	
+
 	@Test
-	public void cannotAccelerateBeyondMaxSpeed(){
+	public void cannotAccelerateBeyondMaxSpeed() {
 		Car car1 = new Car("Red", 100);
 		car1.accelerate(101);
 		assertThat(car1.getSpeed(), is(100));
 	}
-	
-	@Test 
-	public void canDoToString(){
+
+	@Test
+	public void createsAString() {
 		Car car1 = new Car("Red", 100);
 		assertThat(car1.toString(), is("Color: Red, Speed: 0"));
 	}
 
-	// Create and pass test for deceleration.
-	
-	// Create and pass test for no deceleration below 0.
+	@Test
+	public void cannotDecelerateBeyondMinimumSpeed() {
 
-	
+		Car car1 = new Car("Red", 100);
+		car1.decelerate(-1);
+		assertThat(car1.getSpeed(), is(0));
+	}
+
 }
